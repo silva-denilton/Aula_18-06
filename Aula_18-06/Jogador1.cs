90c5e1bc12282472a933bbfc8238ecda8b2c9703
041983 <041983@UNI-LAB3-67>
2024-06-18 20:27:36 -0300

    Add project files.

---
+++ Aula_18-06/Jogador1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_18_06
+{
+    internal class Jogador1
+    {
+        public int Valor1;
+        public int Valor2;
+        public int Valor3;
+
+      public int Media1()
+        {
+            return (Valor1 + Valor2 + Valor3) / 3;
+        }
+
+    }
+   
+}
---
+++ Aula_18-06/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula_18_06
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Jogador1 _joga1 = new Jogador1();
+            Jogador1 _joga2 = new Jogador1();
+            Console.WriteLine("jogador 1");
+
+            var random = new Random();
+            _joga1.Valor1 = random.Next(1, 6);
+            Console.WriteLine(_joga1.Valor1);
+            
+            _joga1.Valor2 = random.Next(1, 6);
+            Console.WriteLine(_joga1.Valor2);
+
+            _joga1.Valor3 = random.Next(1, 6);
+            Console.WriteLine(_joga1.Valor3);
+
+            Console.WriteLine("jogador 2");
+            _joga2.Valor1 = random.Next(1, 6);
+            Console.WriteLine(_joga2.Valor1);
+
+            _joga2.Valor2 = random.Next(1, 6);
+            Console.WriteLine(_joga2.Valor2);
+            
+            _joga2.Valor3 = random.Next(1, 6);
+            Console.WriteLine(_joga2.Valor3);
+
+            if (_joga1.Media1() > _joga2.Media1())
+            {
+                Console.WriteLine("JOGADOR 1 VENCEDOR!!");
+            }
+            else if(_joga1.Media1() < _joga2.Media1())
+            {
+                Console.WriteLine("JOGADOR 2 VENCEDOR!!");
+            }
+            else { Console.WriteLine("!!DEU EMPATE!!"); }
+            Console.ReadKey();
+
+        }
+    }
+}
